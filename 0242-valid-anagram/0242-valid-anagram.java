619ccf994f216d3fcbeada2d486c418ff842b7e9
KEERTHANAGANESHAN <keerthanaganeshan49@gmail.com>
2026-06-23 14:12:48 +0530

    Time: 5 ms (61.97%), Space: 46.6 MB (15.84%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char a[]=s.toLowerCase().toCharArray();
+        char b[]=t.toLowerCase().toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return(Arrays.equals(a,b));
+    }
+}
